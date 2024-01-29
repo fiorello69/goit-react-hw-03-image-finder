d8e2223ee8190fbe5e6f47b1f27f7455878e83a1
Viorel Breban <vbreban69@gmail.com>
2024-01-29 21:46:38 +0100

    third try

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './ImageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ image, onImageClick }) => (
   <li
